671c87f63724955a1c318a63e9f45bc5871827a9
Pavel Tkachuk <pavelt0309@gmail.com>
2017-09-27 00:08:42 +0300

    implemented FSM class

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,54 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (typeof config == 'undefined')
+            throw new Error('config is undefined')
+        this.config = config;
+        this.log = [config['initial']]
+        this.currState = 0;
+        this.canRedo = 0;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.log[this.currState];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (!(state in this.config['states']))
+            throw new Error('impossiple state')
+        this.log.push(state);
+        this.currState++;
+        this.canRedo = 0;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (!(event in this.config.states[this.log[this.currState]].transitions))
+            throw new Error('trigger doesn\'t exists in this state')
+        this.log.push(this.config.states[this.log[this.currState]].transitions[event]);
+        this.currState++;
+        this.canRedo = 0;
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.log.push(this.config['initial']);
+        this.currState++;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +58,45 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        return Object.keys(this.config.states).filter(state => event ? (event in this.config.states[state].transitions) : true);
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.currState == 0)
+            return false;
+        this.currState--;
+        this.canRedo++;
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if (this.canRedo == 0){
+            return false;
+        }
+        this.currState++;
+        this.canRedo--;
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.log = [this.config['initial']];
+        this.currState = 0;
+        this.canRedo = 0;
+    }
 }
 
 module.exports = FSM;
